016cc543bd42a0444260ca95b95d6d96d051a617
Jokler <github@jokler.xyz>
2024-11-09 19:29:49 +0100

    Enable itch.io upload and adjust name

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
 
 use bevy::prelude::*;
-use fishstick_jam_2::AppPlugin;
+use thanks_wife::AppPlugin;
 
 fn main() -> AppExit {
     App::new().add_plugins(AppPlugin).run()
